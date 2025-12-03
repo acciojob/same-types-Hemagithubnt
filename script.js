941971af3f57c8bc14713eb0c69c0cc5636d8df3
Hemant kumar <hmt364134@gmail.com>
2025-12-02 21:54:44 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function isSameType(value1, value2) {
-    // Directly return the boolean result
-    return typeof(value1) === typeof(value2);
+    // Convert strings to numbers if they look like numbers
+    let val1 = isNaN(value1) ? value1 : Number(value1);
+    let val2 = isNaN(value2) ? value2 : Number(value2);
+    
+    return typeof(val1) === typeof(val2);
 }
 
 let value1 = prompt("Enter Start of the Range.");
